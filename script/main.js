f58d01a0efa79b0f02649ff847865d85d27aa52e
Orwa-Jammoul <orwa.jammoul@gmail.com>
2023-10-16 16:32:17 +0300

    ready

--- script/main.js
+++ script/main.js
@@ -1,47 +1,3 @@
-// $(document).ready(function() {
-//   var scrollTime = 100; // Animation duration in milliseconds
-//   var debounceTime = 500; // Time in milliseconds to prevent multiple scroll events in quick succession
-
-//   var isScrolling = false;
-//   var timeout;
-
-//   $(window).on('mousewheel', function(event) {
-//     event.preventDefault();
-
-//     if (!isScrolling) {
-//       isScrolling = true;
-
-//       clearTimeout(timeout);
-
-//       timeout = setTimeout(function() {
-//         isScrolling = false;
-//       }, debounceTime);
-
-//       var delta = event.originalEvent.deltaY;
-
-//       var scrollDirection = delta > 0 ? 'next' : 'prev';
-
-//       var $currentSection = $('.section.active');
-//       var $nextSection;
-
-//       if (scrollDirection === 'next') {
-//         $nextSection = $currentSection.next('.section');
-//       } else {
-//         $nextSection = $currentSection.prev('.section');
-//       }
-
-//       if ($nextSection.length) {
-//         $('html, body').animate({
-//           scrollTop: $nextSection.offset().top
-//         }, scrollTime, function() {
-//           $currentSection.removeClass('active');
-//           $nextSection.addClass('active');
-//         });
-//       }
-//     }
-//   });
-// });
-
 function showNavbarDropDown(){
   $('.drdn-menu').toggleClass('active');
 }
@@ -53,9 +9,80 @@ function goto(link){
   window.location.replace(link);
 }
 
+function smoothScroll(e){
+  let isDownMove= e.originalEvent.deltaY >= 0;
+  let winHeight = window.innerHeight;
+  let scrollPosition=$(window).scrollTop();
+  let sectionNum = parseInt(Math.floor((scrollPosition+10)/winHeight))
+  // console.log(winHeight);
+  let pageBottom=document.body.scrollHeight-winHeight-10;
+  if(scrollPosition>pageBottom){
+    sectionNum+=1;
+  }
+  console.log(pageBottom);
+  console.log(scrollPosition);
+  console.log(sectionNum);
+  if(isDownMove){
+    window.scrollTo(0,(sectionNum+1)* winHeight);
+  }else{
+    window.scrollTo(0, (sectionNum-1)* winHeight);
+  }
+}
+
+function transparenteNavBar(scrollPosition){
+  let navbar= $(".main-navbar .nav-bottom")[0]
+  if(scrollPosition>10 ){
+    navbar.classList.add('solid-bg');
+  }else{
+    navbar.classList.remove('solid-bg');
+  }
+}
+
+let cardIndex=0;
+let playAnimation=true;
+
+function animateCards(){
+  if(playAnimation){
+    const cards=$(".cards .card");
+    const len=cards.length;
+    if(len===1){
+      cards[cardIndex].classList.add("active");
+      return;
+    }
+    let previus= (cardIndex-1 +len)%len;
+    cards[cardIndex].classList.add("active")
+    cards[previus].classList.remove("active")
+    cardIndex=(cardIndex+1)%len;
+  }
+}
+
 
 $(document).ready(function(){
+  cardIndex=0;
+
   $('.drdn-menu').mouseleave(function(){ 
     hideNavbarDropDown();
+  });
+
+  $(window).on('scroll',function(){
+    let scrollPosition=$(window).scrollTop();
+    transparenteNavBar(scrollPosition)
+  });
+
+  $(window).on('mousewheel',function(e){
+    smoothScroll(e)
+  });
+
+
+  $(".cards .card").bind("mouseover",function(){
+    playAnimation=false;
+    const cards=$(".cards .card");
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].classList.remove("active")
+    }
   })
+  $(".cards .card").bind("mouseleave",function(){
+    playAnimation=true;
+  })
+  setInterval(animateCards, 2000);
 });
